07393980a3b633caed87b9876b0e228b5d51bc7c
Mudit0310 <100966335+Mudit0310@users.noreply.github.com>
2023-07-06 01:00:49 +0530

    Create form.js

---
+++ form.js
@@ -0,0 +1,73 @@
+ <script>
+    function openClassForm() {
+      document.getElementById("classFormOverlay").style.display = "flex";
+    }
+
+    function closeClassForm() {
+      document.getElementById("classFormOverlay").style.display = "none";
+    }
+
+    function openTeacherForm() {
+      document.getElementById("teacherFormOverlay").style.display = "flex";
+    }
+
+    function closeTeacherForm() {
+      document.getElementById("teacherFormOverlay").style.display = "none";
+    }
+    
+    function openStudentForm() {
+      document.getElementById("StudentFormOverlay").style.display = "flex";
+    }
+
+    function closeStudentForm() {
+      document.getElementById("StudentFormOverlay").style.display = "none";
+    }
+    
+    function openClassTeacherForm() {
+      document.getElementById("classteacherFormOverlay").style.display = "flex";
+    }
+
+    function closeClassTeacherForm() {
+      document.getElementById("classteacherFormOverlay").style.display = "none";
+    }
+    
+    function openClassDtlsForm() {
+      document.getElementById("ClassDtlsFormOverlay").style.display = "flex";
+    }
+
+    function closeClassDtlsForm() {
+      document.getElementById("ClassDtlsFormOverlay").style.display = "none";
+    }
+    
+    function openteacherattForm() {
+      document.getElementById("teacherattFormOverlay").style.display = "flex";
+    }
+
+    function closeteacherattForm() {
+      document.getElementById("teacherattFormOverlay").style.display = "none";
+    }
+    
+    function openteacherleavedtlsForm() {
+      document.getElementById("teacherleavedtlsFormOverlay").style.display = "flex";
+    }
+
+    function closeteacherleavedtlsForm() {
+      document.getElementById("teacherleavedtlsFormOverlay").style.display = "none";
+    }
+    
+    function openstudentattForm() {
+      document.getElementById("studentattFormOverlay").style.display = "flex";
+    }
+
+    function closestudentattForm() {
+      document.getElementById("studentattFormOverlay").style.display = "none";
+    }
+    
+    function openstudentleavedtlsForm() {
+      document.getElementById("studentleavedtlsFormOverlay").style.display = "flex";
+    }
+
+    function closestudentleavedtlsForm() {
+      document.getElementById("studentleavedtlsFormOverlay").style.display = "none";
+    }
+  </script>
